ec25e2b843fb7be6c8cb2c2c4bc8f53867bf5e2b
eronyako <1138345264@qq.com>
2022-09-05 18:48:34 +0800

    v1.1

--- TeleGraphSpider.py
+++ TeleGraphSpider.py
@@ -9,12 +9,12 @@ import os
 import sys
 import re
 import requests
+from bs4 import BeautifulSoup
 
 url_root = 'https://telegra.ph/'
-img_root = url_root + 'file/'
 headers_def = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
 title_re = 'class="tl_article_content"><h1>(.*?)<br></h1><address>'
-img_re = '<img src="/file/(.*?)">'
+img_re = '<img src="/(.*?)">'
 
 
 def get_graph_url():
@@ -37,7 +37,8 @@ def get_graph_url():
     return urls
 
 
-def get_pic_url(url: str, headers: dict):
+# 正则式方式获取名称与图片链接
+def get_pic_url_re(url: str, headers: dict):
     # 获取页面内容
     res = requests.get(url=url, headers=headers).text
     # 取得标题
@@ -45,7 +46,23 @@ def get_pic_url(url: str, headers: dict):
     # 找到图片名称
     url_list = re.findall(img_re, res, re.S)
     # 将名称加上前缀转为 url
-    url_list = list(map(lambda v: img_root + v, url_list))
+    url_list = list(map(lambda v: url_root + v, url_list))
+    return title, url_list
+
+
+# bs4 方式获取名称与图片链接
+def get_pic_url_bs4(url: str, headers: dict):
+    # 获取页面内容
+    res = BeautifulSoup(requests.get(url=url, headers=headers).text, 'lxml')
+    # 取得标题
+    title = res.select('.tl_article_header > h1')[0].text
+    # 找到图片名称
+    url_list_soup = res.select('#_tl_editor > img')
+    url_list = []
+    for v in url_list_soup:
+        url_list.append(v['src'].strip('/'))
+    # 将名称加上前缀转为 url
+    url_list = list(map(lambda i: url_root + i, url_list))
     return title, url_list
 
 
@@ -79,7 +96,7 @@ def main():
     # 对每个 telegraph url 列表循环获取
     for v in graph_list:
         # 取得名字和图片列表
-        hon_name, img_list = get_pic_url(v, headers_def)
+        hon_name, img_list = get_pic_url_re(v, headers_def)
         # 获取图片
         get_img(hon_name, img_list, headers_def, lib_path)
 
