d96c5b64e5f38b52012d71222decd37603c1c431
eronyako <1138345264@qq.com>
2022-09-05 16:27:46 +0800

    v1.0

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # TeleGraphSpider
+
 本项目为使用 python3 编写的 TeleGraph 爬虫工具。
+
+## 使用
+本项目可以使用直接使用 .py 文件（需要安装 Python3 并添加到环境变量），或使用 Releases 的 exe 文件。
+
+所需文件如下：
+
+- 程序主体：TeleGraphSpider.py / TeleGraphSpider.exe
+- url 列表文件
+
+### 密码字典要求
+
+url 列表文件需要使用 UTF-8 Without BOM 编码。
+
+### 使用源码
+
+拖动 url 列表文件件到 TeleGraphSpider.py
+
+直接运行的情况，将会读取当前文件夹下的 urls.txt
+
+在当前目录运行：
+
+```shell
+python TeleGraphSpider.py
+```
+
+#### 打包 exe 文件
+
+可使用 pyinstaller 包，用如下命令将会在 `./dist/` 目录下创建 windows 可执行程序：
+
+```shell
+pyinstaller -F --icon=icon.ico TeleGraphSpider.py
+```
+
+### 使用封装版
+
+拖动 url 列表文件到  TeleGraphSpider.exe
+
+直接运行的情况，将会读取当前文件夹下的 urls.txt
+
+## 输出
+
+若正常运行后将会在当前文件夹创建 telegraph_lib 文件夹。
+
+url 列表文件中每个 url 会按照标题在 telegraph_lib 文件夹中创建文件夹，每个 url 中的图片将被下载到该文件夹中。
---
+++ TeleGraphSpider.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# @Subj: PyCharm
+# @File: TeleGraphSpider.py
+# @Date: 2022/9/5 13:14
+
+import os
+import sys
+import re
+import requests
+
+url_root = 'https://telegra.ph/'
+img_root = url_root + 'file/'
+headers_def = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36'}
+title_re = 'class="tl_article_content"><h1>(.*?)<br></h1><address>'
+img_re = '<img src="/file/(.*?)">'
+
+
+def get_graph_url():
+    # 取得 url 文件路径
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        print('未拖动文件打开，尝试 urls.txt')
+        if os.path.isfile(os.path.join(os.getcwd(), 'urls.txt')):
+            file_name = os.path.join(os.getcwd(), 'urls.txt')
+        else:
+            raise FileNotFoundError('未找到 urls.txt')
+
+    # 读取 url 文件生成列表
+    with open(file_name, 'r', encoding='UTF-8') as f:
+        urls = f.readlines()
+    urls = list(map(lambda v: v.strip(), urls))
+    while '' in urls:
+        urls.remove('')
+    return urls
+
+
+def get_pic_url(url: str, headers: dict):
+    # 获取页面内容
+    res = requests.get(url=url, headers=headers).text
+    # 取得标题
+    title = re.findall(title_re, res, re.S)[0]
+    # 找到图片名称
+    url_list = re.findall(img_re, res, re.S)
+    # 将名称加上前缀转为 url
+    url_list = list(map(lambda v: img_root + v, url_list))
+    return title, url_list
+
+
+def get_img(title: str, url_list: list, headers: dict, path: str):
+    # 判断是否出现重名目录
+    if not os.path.exists(os.path.join(path, title)):
+        os.mkdir(os.path.join(path, title))
+        # 通过 i 计数，使图片名称序列化
+        i = 0
+        for v in url_list:
+            i += 1
+            # 取得图片内容
+            img = requests.get(v, headers=headers).content
+            # 整理图片名
+            img_name = format(i, '03d') + '.' + v.split('.')[-1]
+            # 写入硬盘
+            with open(os.path.join(path, title, img_name), 'wb') as f:
+                f.write(img)
+    else:
+        # 若重名，则不保存
+        print(f'文件夹已存在：{title}')
+
+
+def main():
+    # 创建 telegraph_lib 目录
+    lib_path = os.path.join(os.getcwd(), 'telegraph_lib')
+    if not os.path.exists(lib_path):
+        os.mkdir(lib_path)
+
+    graph_list = get_graph_url()
+    # 对每个 telegraph url 列表循环获取
+    for v in graph_list:
+        # 取得名字和图片列表
+        hon_name, img_list = get_pic_url(v, headers_def)
+        # 获取图片
+        get_img(hon_name, img_list, headers_def, lib_path)
+
+
+if __name__ == '__main__':
+    main()
